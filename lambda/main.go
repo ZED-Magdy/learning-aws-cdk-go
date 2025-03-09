78572a2c54a7e51877d513b281529a52c17ef20f
ZED-Magdy <zedmagdy@gmail.com>
2025-03-09 03:46:12 +0200

    feat: update Lambda handler to process JSON input and return formatted response

--- lambda/main.go
+++ lambda/main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 
-type MyEvent struct {
-	Name string `json:"name"`
-}
+func handler(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var body map[string]string
+	if err := json.Unmarshal([]byte(req.Body), &body); err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusBadRequest,
+			Body:       "Invalid request body",
+		}, nil
+	}
 
-func handler(event MyEvent) (string, error) {
-	return fmt.Sprintf("Hello %s!", event.Name), nil
+	name, ok := body["name"]
+	if !ok {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusBadRequest,
+			Body:       "Missing name in request body",
+		}, nil
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Body:       fmt.Sprintf("Hello, %s!", name),
+	}, nil
 }
 
 func main() {
